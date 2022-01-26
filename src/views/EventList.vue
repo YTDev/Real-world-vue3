1556b30643a70e2c577c4438ecb87d749ad78020
YLEB <meYLEB@example.com>
2022-01-26 13:57:35 +0200

    first commit

---
+++ src/views/EventList.vue
@@ -0,0 +1,111 @@
+<template>
+  <h1>Events For Good</h1>
+  <div class="events">
+    <EventCard v-for="event in events" :key="event.id" :event="event" />
+
+      
+    <div class="pagination">
+
+      <router-link 
+      id="page-prev"
+      :to="{name: 'EventList', query: {page: page - 1}}"
+      rel="prev"
+      v-if="page != 1">&#60; Previous</router-link>
+
+      <router-link 
+      id="page-next"
+      :to="{name: 'EventList', query: {page: page + 1}}"
+      rel="next"
+      v-if="hasNextPage"
+      >Next &#62;</router-link>
+
+    </div>
+    
+    
+     
+  </div>
+</template>
+
+<script>
+// @ is an alias to /src
+import EventCard from '@/components/EventCard.vue'
+import EventService from '@/services/EventService.js'
+import { watchEffect } from 'vue'
+
+export default {
+   
+  name: 'EventList',
+  props: ['page'],
+  components: {
+    EventCard,
+  },
+  data() {
+    return {
+      events: null,
+      totalEvents: 0
+    }
+  },
+  created() {
+    
+    // let f1=()=>console.log(this.events);//in arrow functions this 
+    // f1();
+    // let self=this;
+    // let f2=function(){
+    //   return  console.log(self.events)
+    // };
+    // f2();
+    //in vue here this automaticaly mean this.data so we can access data propreties
+    //but inside a regular funtion this is undefind, with the help of arrow functions
+    //this refer to created() { this } so that's good
+    watchEffect(() => {
+      this.events = null;
+      EventService.getEvents(2, this.page)
+        // .get(
+        //   'https://my-json-server.typicode.com/Code-Pop/Real-World_Vue-3/events'
+        // )
+        .then(response => {
+          this.events = response.data;
+          this.totalEvents = response.headers['x-total-count'];
+          // console.log(this.totalEvents)
+        })
+        .catch(error => {
+          console.log(error);
+        })
+    })
+  },
+  computed: {
+
+    hasNextPage() {
+      var totalPages = Math.ceil(this.totalEvents / 2)
+      console.log(totalPages);
+      return this.page < totalPages;
+    }
+  }
+}
+</script>
+
+<style scoped>
+.events {
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+}
+
+.pagination{
+  display: flex;
+  width: 290px;
+}
+.pagination a{
+  flex: 1;
+  text-decoration: none;
+  color: #2c3e50;
+}
+#page-prev{
+  text-align: left;
+}
+#page-next{
+  text-align: right;
+}
+
+
+</style>
