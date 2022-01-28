9e19de1bf984cd946e718035a2b0e372b84f12a2
YLEB <meYLEB@example.com>
2022-01-28 21:51:18 +0200

    Programmatic navigation

--- src/views/event/Register.vue
+++ src/views/event/Register.vue
@@ -1,13 +1,32 @@
 <template>
 
     <p>Register for the event here.</p>
-
+    <button @click="register">Register Me</button>
 </template>
 
 <script>
     
     export default {
-        props: ['event']
+        props: ['event'],
+        methods: {
+            register() {
+                //Call to api
+                //If registered then redirect to event details
+
+                this.$router.push({
+                    name: "EventDetails",
+                    // since we are on the registerd component we don't need the id
+                    // params: { id: this.event.id }
+                })
+
+                // this.$router.replace({
+                //     name: 'EventDetails',
+                //     // params: { id: 3}
+                // });
+
+                
+            }
+        }
     }
     
 </script>
